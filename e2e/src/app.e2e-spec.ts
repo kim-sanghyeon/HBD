115457eec89695a71bf14b05c0508d3c2bd7ca5e
kimsanghyeon <hyeon@seoulpass.kr>
2023-08-25 19:22:19 +0900

    프로젝트명 수정

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('HBDDD app is running!');
+    expect(page.getTitleText()).toEqual('HBD app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'HBDDD'`, () => {
+  it(`should have as title 'HBD'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('HBDDD');
+    expect(app.title).toEqual('HBD');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('HBDDD app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('HBD app is running!');
   });
 });
